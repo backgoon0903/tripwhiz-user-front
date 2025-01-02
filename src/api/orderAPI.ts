1e35aab382d3492d0ae7139d3203c1497c9b6287
eggzuxi <eggzuxi99@gmail.com>
2025-01-02 19:37:03 +0900

    local_JH

--- src/api/orderAPI.ts
+++ src/api/orderAPI.ts
@@ -15,11 +15,11 @@ const getEmailFromAuthStore = () => {
 };
 
 // API 기본 URL 설정
-const USER_BASE_URL = "/api/user/order"; // 유저 백엔드 URL
-// const USER_BASE_URL = "http://localhost:8081/api/user/order"; // 유저 백엔드 URL
+// const USER_BASE_URL = "/api/user/order"; // 유저 백엔드 URL
+const USER_BASE_URL = "http://localhost:8081/api/user/order"; // 유저 백엔드 URL
 
-const STORE_BASE_URL = "/api/storeowner/order"; // 점주 백엔드 URL
-// const STORE_BASE_URL = "http://localhost:8081/api/storeowner/order"; // 점주 백엔드 URL
+// const STORE_BASE_URL = "/api/storeowner/order"; // 점주 백엔드 URL
+const STORE_BASE_URL = "http://localhost:8081/api/storeowner/order"; // 점주 백엔드 URL
 
 
 // 주문 생성 함수
@@ -27,7 +27,7 @@ const STORE_BASE_URL = "/api/storeowner/order"; // 점주 백엔드 URL
 
 export const createOrder = async (email: string, spno: number, pickUpDate: string) => {
     const response = await axios.post(
-        "https://tripwhiz.shop/api/user/order/create",
+        "http://localhost:8081/api/user/order/create",
         null,
         {
             headers: {
